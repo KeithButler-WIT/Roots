fee8fc2ad9f0808a3f339078d3478140041d2b52
Keith Butler <20089137@mail.wit.ie>
2023-02-04 17:59:25 +0000

    Update

--- Scripts/Item.gd
+++ Scripts/Item.gd
@@ -1,13 +1,13 @@
-extends Node2D
+extends KinematicBody2D
 
 var isTarget : bool = false
 
 func _ready():
-	#deleteCurrency()
+	#deleteItem()
 	pass
 
 
-func deleteCurrency():
+func deleteItem():
 	# Add a timer to this node
 	var timer = Timer.new()
 	self.add_child(timer)
@@ -18,3 +18,22 @@ func deleteCurrency():
 	timer.start()
 	
 #	queue_free()
+
+var velocity = Vector2()
+
+func _on_RigidBody2D_mouse_entered():
+	var speed = Input.get_last_mouse_speed();
+
+	velocity = Vector2()
+	if speed.x:
+		velocity.x += speed
+	if speed.x < 0:
+		velocity.x -= speed
+	if speed.y > 0:
+		velocity.y += speed
+	if speed.y < 0:
+		velocity.y -= speed
+	velocity = velocity.normalized() * speed
+
+func _physics_process(delta):
+	velocity = move_and_slide(velocity)
